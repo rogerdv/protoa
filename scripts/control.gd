688d7f072659063d457efde79dd5e33f1f94c87d
DiedByDisgust <sergio.cabrerafalcon@gmail.com>
2022-11-18 04:40:20 -0500

    basic control for navitation click movement

---
+++ scripts/control.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var Lclick:Vector3
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,18 +3,9 @@ extends entity
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super()
-	#test purpose
-	nav_agent.set_target_location($"../target2".global_position)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-#Test purpose PRESS "P"
-func _input(event):
-	if Input.is_key_pressed(KEY_P):
-		if nav_agent.target_location == $"../target".global_position:
-			nav_agent.set_target_location($"../target2".global_position)
-		else:
-			nav_agent.set_target_location($"../target".global_position)
+	#test purpose
+	if GlobalControl.Lclick != Vector3.ZERO:
+		nav_target(GlobalControl.Lclick)
