c91989872909a61cec67c645d1398d549a66c6db
Roger <rogerdv@gmail.com>
2022-11-24 19:51:13 -0500

    Added some basic members

--- scripts/game/ability_base.gd
+++ scripts/game/ability_base.gd
@@ -2,5 +2,11 @@ extends Resource
 class_name ability_base
 
 @export var id:String
+@export var fx:Resource	#The visual effect, like a fireball or so
+# Abilities may have a cast sound (played while you cast)
+# and an hit sound (played when spell hits target)
+@export var cast_sound:Resource
+@export var hit_sound:Resource
 
-
+func use(owner, target):
+	pass
