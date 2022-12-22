2dc0c5c668c80caa1b2e1144a20f53536f42ba97
rogerdv <rogerdv@gmail.com>
2022-12-22 11:12:44 -0500

    Added  a tester scene, to try items on player

---
+++ scripts/item_tester.gd
@@ -0,0 +1,40 @@
+extends Node3D
+
+var player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player= game_instance.player
+	add_child(player)
+	update_item_list()
+
+func update_item_list():
+	for it in game_instance.items:
+		var line = HBoxContainer.new()
+		var l=Label.new()
+		l.text=tr(it.id)
+		line.add_child(l)
+		var add_b = Button.new()
+		add_b.text="equip"
+		add_b.pressed.connect(equip.bind(it))
+		line.add_child(add_b)
+		$ui/items.add_child(line)
+
+func equip(it):
+	# TRansfer to inventory
+	if player.inventory.has(it.id):
+		#player has a similar item
+		if it.stack:
+			game_instance.player.inventory[it.id]["amount"]+=1
+		else :
+			var data= {"amount":1,"quality":1,"item":it}
+			game_instance.player.inventory[it.id]=data
+	else :
+		var data= {"amount":1,"quality":1,"item":it}
+		game_instance.player.inventory[it.id]=data
+	player.inventory[it.id]["item"].equip(player)
+	player.equip[it.slot]=it.id
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
