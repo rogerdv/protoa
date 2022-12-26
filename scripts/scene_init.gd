0be89341a3aedf6696d25994fda31f1dbccab42f
Roger <rogerdv@gmail.com>
2022-12-25 22:19:08 -0500

    Initial implementation of scene switching

--- scripts/scene_init.gd
+++ scripts/scene_init.gd
@@ -4,3 +4,8 @@ func _ready():
 	GlobalControl.scene_ui = $UI	
 	add_child(game_instance.player)
 	
+
+func _on_exit_exit_scene(next_scene):
+	remove_child(get_node("player"))
+	get_tree().change_scene_to_file(next_scene)
+	
