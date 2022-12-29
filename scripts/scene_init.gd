5e631d8d9af307d0350cb5e21b2aa54cd77bb15e
Roger <rogerdv@gmail.com>
2022-12-28 21:58:00 -0500

    Fix: scene change now works (TODO: needs to reset some stuff)

--- scripts/scene_init.gd
+++ scripts/scene_init.gd
@@ -3,7 +3,7 @@ extends Node3D
 func _ready():
 	GlobalControl.scene_ui = $UI	
 	add_child(game_instance.player)
-	
+	get_node("player").position=Vector3(0,0,0)
 
 func _on_exit_exit_scene(next_scene):
 	remove_child(get_node("player"))
